278232cb7bac8d0b4f87d3839330795f9a235440
prantokhaan <prantokhaan@gmail.com>
2021-09-26 07:55:40 +0600

    solved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Movies from './components/Movies/Movies';
 
 function App() {
   return (
-    <div >
-      
+    <div>
+      <Header />
+      <Movies />
     </div>
   );
 }
